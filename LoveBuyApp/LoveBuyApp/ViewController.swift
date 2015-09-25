c5742718fa5bb9f6b1ef987ed8f450e1f2a87632
kekeye <374055998@qq.com>
2015-09-25 11:49:17 +0800

    commit项目
    commit项目

---
+++ LoveBuyApp/LoveBuyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveBuyApp
+//
+//  Created by Daniel on 15/9/25.
+//  Copyright © 2015年 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
